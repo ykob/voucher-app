459433e5a6412e6c83ac866874b1363faa561418
ykob <info@tplh.net>
2024-07-07 23:16:37 +0900

    update vite config on web.

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -3,4 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [remix()],
+  resolve: {
+    alias: {
+      "~": `${__dirname}/app/`,
+      "styled-system": `${__dirname}/styled-system/`,
+    },
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
