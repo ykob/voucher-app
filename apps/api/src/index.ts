f1bcc20e7094192232919ea92f56a6b5b311497d
ykob <info@tplh.net>
2024-07-08 23:36:37 +0900

    changed api port to 4000.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,8 +7,8 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 4000
+console.log(`API Server is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
