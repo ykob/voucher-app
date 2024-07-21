d63def923d267dd5e3031b0e6d9a58ddf529602f
ykob <info@tplh.net>
2024-07-21 11:04:17 +0900

    added AppType on api root.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,16 +3,14 @@ import { Hono } from 'hono';
 import { estimate } from './routes';
 
 const app = new Hono();
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-});
-app.route('/estimate', estimate);
-
+const routes = app.route('/estimate', estimate);
 const port = 4000;
+
 console.log(`API Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
   port,
 });
+
+export type AppType = typeof routes;
