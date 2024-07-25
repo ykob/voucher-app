ed05c628e1152563e94ed3bc462b1ea149dc4ff3
ykob <info@tplh.net>
2024-07-25 23:02:44 +0900

    added auth route on api.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { estimates } from './routes';
+import { auth, estimates } from './routes';
 
 const app = new Hono();
-const routes = app.route('/estimates', estimates);
+const routes = app.route('/auth', auth).route('/estimates', estimates);
 const port = 4000;
 
 console.log(`API Server is running on port ${port}`);
