fb36f66a35c99f239b23b1a6c338c668255a3bf9
ykob <info@tplh.net>
2024-08-15 20:40:04 +0900

    update eslint configs for web.

--- packages/eslint-config/web.js
+++ packages/eslint-config/web.js
@@ -1,1 +1,18 @@
-export default [];
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat({});
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...compat.extends('plugin:turbo/recommended'),
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+  },
+];
