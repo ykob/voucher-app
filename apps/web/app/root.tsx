6439ddb3ecde57e497927621d3862ad6e6f3206d
ykob <info@tplh.net>
2024-08-26 00:04:35 +0900

    removed liveReload.

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,7 +1,6 @@
 import { type LinksFunction } from '@remix-run/node';
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -23,7 +22,6 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
