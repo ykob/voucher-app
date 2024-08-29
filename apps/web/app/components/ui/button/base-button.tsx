984d2ac7340b806fdd58b257e01e91b7483de147
ykob <info@tplh.net>
2024-08-29 23:15:08 +0900

    update base button component to add classname prop.

--- apps/web/app/components/ui/button/base-button.tsx
+++ apps/web/app/components/ui/button/base-button.tsx
@@ -1,10 +1,14 @@
 import { ComponentProps } from 'react';
-import { css } from 'styled-system/css';
+import { css, cx } from 'styled-system/css';
 
 type BaseButtonProps = ComponentProps<'button'>;
 
 export function BaseButton({ children, className, ...props }: BaseButtonProps) {
-  return <button {...props}>{children}</button>;
+  return (
+    <button className={cx(styles.container, className)} {...props}>
+      {children}
+    </button>
+  );
 }
 
 const styles = {
