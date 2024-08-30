df0555074932c38cf09d90d1da88f1cc8b252b43
ykob <info@tplh.net>
2024-08-30 17:50:12 +0900

    added asChild pattern.

---
+++ apps/web/app/components/utils/as-child.tsx
@@ -0,0 +1,34 @@
+import {
+  Children,
+  HTMLAttributes,
+  ReactNode,
+  cloneElement,
+  isValidElement,
+} from 'react';
+
+export type AsChildProps<T> =
+  | ({
+      asChild: true;
+    } & T)
+  | {
+      asChild?: false;
+      children: ReactNode;
+    };
+
+export function Slot({
+  children,
+  ...props
+}: HTMLAttributes<HTMLElement> & {
+  children?: ReactNode;
+}) {
+  if (isValidElement(children)) {
+    return cloneElement(children, {
+      ...props,
+      ...children.props,
+    });
+  }
+  if (Children.count(children) > 1) {
+    Children.only(null);
+  }
+  return null;
+}
