243aa33780087766624e2488c34aa46a6160041e
ykob <info@tplh.net>
2024-09-01 18:08:31 +0900

    added container query for main layout component.

--- apps/web/app/routes/_main/components/layout.tsx
+++ apps/web/app/routes/_main/components/layout.tsx
@@ -1,8 +1,33 @@
+import { css } from 'styled-system/css';
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <h1>Voucher App</h1>
-      {children}
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <h1 className={styles.heading}>Voucher App</h1>
+        {children}
+      </div>
     </div>
   );
 }
+
+const styles = {
+  container: css({
+    containerName: 'layout',
+    containerType: 'inline-size',
+    maxW: '800px',
+    mx: 'auto',
+  }),
+  innerContainer: css({
+    px: 5,
+    py: 10,
+    '@container layout (width > 640px)': {
+      px: 10,
+      py: 12,
+    },
+  }),
+  heading: css({
+    fontSize: '3xl',
+    mb: 4,
+  }),
+};
