275df617dceb657c7ef8a736c9e8ce9339a33488
ykob <info@tplh.net>
2024-09-16 09:20:48 +0900

    update filled button story.

--- apps/web/app/components/ui/button/filled-button.stories.tsx
+++ apps/web/app/components/ui/button/filled-button.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import { FilledButton } from './filled-button';
 
 const meta: Meta<typeof FilledButton> = {
+  title: 'UI/Button/FilledButton',
   component: FilledButton,
 };
 
@@ -10,13 +11,63 @@ export default meta;
 type Story = StoryObj<typeof FilledButton>;
 
 export const FilledButtonStory: Story = {
+  name: 'FilledButton',
   render: () => (
     <div>
       <h1 className="story-heading">FilledButton Component</h1>
-      <div className="flex">
+      <h2 className="story-heading">Primary</h2>
+      <div className="story-flex">
         <FilledButton buttonSize="sm">Button</FilledButton>
         <FilledButton buttonSize="md">Button</FilledButton>
         <FilledButton buttonSize="lg">Button</FilledButton>
+      </div>
+      <h2 className="story-heading">Secondary</h2>
+      <div className="story-flex">
+        <FilledButton buttonSize="sm" buttonColor="secondary">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="md" buttonColor="secondary">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="lg" buttonColor="secondary">
+          Button
+        </FilledButton>
+      </div>
+      <h2 className="story-heading">Success</h2>
+      <div className="story-flex">
+        <FilledButton buttonSize="sm" buttonColor="success">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="md" buttonColor="success">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="lg" buttonColor="success">
+          Button
+        </FilledButton>
+      </div>
+      <h2 className="story-heading">Warning</h2>
+      <div className="story-flex">
+        <FilledButton buttonSize="sm" buttonColor="warning">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="md" buttonColor="warning">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="lg" buttonColor="warning">
+          Button
+        </FilledButton>
+      </div>
+      <h2 className="story-heading">Danger</h2>
+      <div className="story-flex">
+        <FilledButton buttonSize="sm" buttonColor="danger">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="md" buttonColor="danger">
+          Button
+        </FilledButton>
+        <FilledButton buttonSize="lg" buttonColor="danger">
+          Button
+        </FilledButton>
       </div>
     </div>
   ),
