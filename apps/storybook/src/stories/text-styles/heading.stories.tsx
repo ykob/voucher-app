767d5fd1f5a4d36b5b9240822e846ee338c2413a
ykob <info@tplh.net>
2024-09-22 10:08:45 +0900

    update heading text styles.

--- apps/storybook/src/stories/text-styles/heading.stories.tsx
+++ apps/storybook/src/stories/text-styles/heading.stories.tsx
@@ -12,9 +12,39 @@ export const FilledButtonStory: StoryObj = {
   render: () => (
     <div>
       <h1 className="story-heading">Heading Text Styles</h1>
-      <h1 className={css({ textStyle: 'heading.lg' })}>Heading Large</h1>
-      <h1 className={css({ textStyle: 'heading.md' })}>Heading Medium</h1>
-      <h2 className={css({ textStyle: 'heading.sm' })}>Heading Small</h2>
+      <div className="story-flex-column">
+        <h2 className={css({ textStyle: 'heading.primary' })}>
+          Heading Primary
+        </h2>
+        <p>
+          In a small town, nestled between rolling hills and vibrant fields,
+          there lived a curious cat named Whiskers. Whiskers was no ordinary
+          cat; he had an adventurous spirit and a keen sense of exploration.
+          Every morning, he would stretch his paws and set off on a new journey,
+          discovering hidden paths and secret gardens. The townsfolk adored him,
+          often leaving out little treats for his return.
+        </p>
+        <h2 className={css({ textStyle: 'heading.secondary' })}>
+          Heading Secondary
+        </h2>
+        <p>
+          One sunny afternoon, Whiskers stumbled upon an old, forgotten well at
+          the edge of the woods. Intrigued, he peered inside and noticed a faint
+          glow emanating from the depths. Without hesitation, he decided to
+          investigate further. As he climbed down cautiously, he found himself
+          in a magical underground world filled with sparkling crystals and
+          whimsical creatures. The inhabitants welcomed him warmly, and together
+          they shared stories and adventures.
+        </p>
+        <h2 className={css({ textStyle: 'heading.tertiary' })}>
+          Heading Tertiary
+        </h2>
+        <p>
+          Whiskers realized that every day held the potential for new
+          discoveries. He returned to the surface with a heart full of joy and
+          stories to tell, knowing that his explorations had only just begun.
+        </p>
+      </div>
     </div>
   ),
 };
--- apps/web/pandacss/theme/text-styles/heading.ts
+++ apps/web/pandacss/theme/text-styles/heading.ts
@@ -2,26 +2,27 @@ import { type TextStyles } from '@pandacss/types';
 
 const textStyleBase = {
   lineHeight: 1.2,
+  fontWeight: 'bold',
   letterSpacing: 0,
 };
 
 export const heading: TextStyles = {
-  lg: {
+  primary: {
     value: {
       ...textStyleBase,
       fontSize: '3xl',
     },
   },
-  md: {
+  secondary: {
+    value: {
+      ...textStyleBase,
+      fontSize: '2xl',
+    },
+  },
+  tertiary: {
     value: {
       ...textStyleBase,
       fontSize: 'xl',
-    },
-  },
-  sm: {
-    value: {
-      ...textStyleBase,
-      fontSize: 'lg',
     },
   },
 };
