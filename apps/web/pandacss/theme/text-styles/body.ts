56c0462c88a1c124801069e6d83f12c9f2e6b8da
ykob <info@tplh.net>
2024-09-22 15:51:14 +0900

    added body text style.

---
+++ apps/web/pandacss/theme/text-styles/body.ts
@@ -0,0 +1,28 @@
+import { type TextStyles } from '@pandacss/types';
+
+const textStyleBase = {
+  lineHeight: 1.75,
+  fontWeight: 'normal',
+  letterSpacing: 0.08,
+};
+
+export const body: TextStyles = {
+  primary: {
+    value: {
+      ...textStyleBase,
+      fontSize: 'md',
+    },
+  },
+  secondary: {
+    value: {
+      ...textStyleBase,
+      fontSize: 'sm',
+    },
+  },
+  tertiary: {
+    value: {
+      ...textStyleBase,
+      fontSize: 'xs',
+    },
+  },
+};
--- apps/web/pandacss/theme/text-styles/index.ts
+++ apps/web/pandacss/theme/text-styles/index.ts
@@ -1,6 +1,8 @@
 import { defineTextStyles } from '@pandacss/dev';
+import { body } from './body';
 import { heading } from './heading';
 
 export const textStyles = defineTextStyles({
   heading,
+  body,
 });
