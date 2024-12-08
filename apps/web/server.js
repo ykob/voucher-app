af07ef970fbf3f60cb9e536eb678274b59d2d13e
ykob <info@tplh.net>
2024-12-08 22:13:38 +0900

    update for development mode.

--- apps/web/server.js
+++ apps/web/server.js
@@ -1,13 +1,24 @@
 import { createRequestHandler } from '@remix-run/express';
 import express from 'express';
 
-// notice that the result of `remix vite:build` is "just a module"
-import * as build from './build/server/index.js';
+const viteDevServer =
+  process.env.NODE_ENV === 'production'
+    ? null
+    : await import('vite').then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        }),
+      );
 
 const app = express();
-app.use(express.static('build/client'));
+app.use(
+  viteDevServer ? viteDevServer.middlewares : express.static('build/client'),
+);
 
-// and your app is "just a request handler"
+const build = viteDevServer
+  ? () => viteDevServer.ssrLoadModule('virtual:remix/server-build')
+  : await import('./build/server/index.js');
+
 app.all('*', createRequestHandler({ build }));
 
 app.listen(3000, () => {
